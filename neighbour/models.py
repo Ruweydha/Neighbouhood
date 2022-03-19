c241f67240db471d0f0a8359adb2e6ad460df72b
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-19 23:47:38 +0300

    Created neighbourhood, police and healthdepartment models

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,22 @@
+import email
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+    Admin  = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Police(models.Model):
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    contact = models.CharField(max_length=10)
+    email = models.EmailField()
+    alternative_contact = models.CharField(max_length=10)
+
+class HealthDepartment(models.Model):
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    contact = models.CharField(max_length=10)
+    email = models.EmailField()
+    alternative_contact = models.CharField(max_length=10)        
+
