5ab77129319256c9f438b39a21397cedcaacfc65
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-19 23:46:51 +0300

    Registred some models

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Neighbourhood, Police, HealthDepartment
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Police)
+admin.site.register(HealthDepartment)
