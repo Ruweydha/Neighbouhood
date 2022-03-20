f281fbbe255613fb6801cb963a8a3b885af6e0af
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-20 05:20:42 +0300

    Created business model, posts models and occupants model

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,6 +1,7 @@
-import email
+from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import CharField
 
 # Create your models here.
 class Neighbourhood(models.Model):
@@ -8,15 +9,55 @@ class Neighbourhood(models.Model):
     location = models.CharField(max_length=30)
     Admin  = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
+    def create_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        Neighbourhood.objects.filter(id = self.id).delete()        
+    
+
 class Police(models.Model):
+    name = models.CharField(max_length=30)
     neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
     contact = models.CharField(max_length=10)
     email = models.EmailField()
     alternative_contact = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 class HealthDepartment(models.Model):
+    name = models.CharField(max_length=30)    
     neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
     contact = models.CharField(max_length=10)
     email = models.EmailField()
-    alternative_contact = models.CharField(max_length=10)        
+    alternative_contact = models.CharField(max_length=10) 
 
+    def __str__(self):
+        return self.name
+
+class Occupants(models.Model):
+    name = CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    id_number = models.CharField(max_length=8)
+    email = models.EmailField()
+    profile_pic = models.ImageField(upload_to = 'profile')
+    location = models.CharField(max_length=30)
+    about_me = models.CharField(max_length=150)
+
+class Businesses(models.Model):
+    name = models.CharField(max_length=30, default='max')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+class Posts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    post = models.CharField(max_length=300)
