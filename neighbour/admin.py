eb946e6b45f8fcb311d64a85319bf3937bfd83fe
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-20 05:20:12 +0300

    Registered business model

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Neighbourhood, Police, HealthDepartment
+from .models import Neighbourhood, Police, HealthDepartment, Businesses
 
 # Register your models here.
 admin.site.register(Neighbourhood)
 admin.site.register(Police)
 admin.site.register(HealthDepartment)
+admin.site.register(Businesses)
