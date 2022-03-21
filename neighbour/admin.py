821a51a34ae4c864faa0b5a431fd1b96644b37c7
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-21 05:34:14 +0300

    Registered occupants and businesses

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Neighbourhood, Police, HealthDepartment, Businesses
+from .models import Neighbourhood, Police, HealthDepartment, Businesses, Posts, Occupants
 
 # Register your models here.
 admin.site.register(Neighbourhood)
 admin.site.register(Police)
 admin.site.register(HealthDepartment)
 admin.site.register(Businesses)
+admin.site.register(Posts)
+admin.site.register(Occupants)
