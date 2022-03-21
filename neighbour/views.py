441730403ecf2236561903b19a4e575a6c1312d1
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-21 05:50:22 +0300

    Added view for displaying searched businesses

--- neighbour/views.py
+++ neighbour/views.py
@@ -98,3 +98,15 @@ def profile(request):
     }
 
     return render(request, 'profile.html', context)
+
+def search_business(request):
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        searched_business = Businesses.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"businesses": searched_business})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})    
